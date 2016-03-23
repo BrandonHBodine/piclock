181e7373e9630cfa8f375094328c3f25aebcb93a
Brandon H Bodine <brandonhbodine@gmail.com>
2016-03-22 21:51:09 -0600

    Fixed vars, they were declared with commas so updated them with best practices

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
-var Gpio = require('onoff').Gpio, // Constructor function for Gpio objects.
+var Gpio = require('onoff').Gpio;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    led = new Gpio(14, 'out'), // Export GPIO #14 as an output.
-    iv;
+  var led = new Gpio(14, 'out'); // Export GPIO #14 as an output.
+  var iv;
 
   // Toggle the state of the LED on GPIO #14 every 200ms.
   // Here synchronous methods are used. Asynchronous methods are also available.
   iv = setInterval(function() {
-    led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
+    led.writeSync(led.readSync() ^ 1);
   }, 200);
 
   // Stop blinking the LED and turn it off after 5 seconds.
