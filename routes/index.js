ab608f0816e817438a05125ef4130bc050416cfb
Brandon H Bodine <brandonhbodine@gmail.com>
2016-03-22 21:41:58 -0600

    Added some test scripts to the routes to see if they will be affected

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Gpio = require('onoff').Gpio, // Constructor function for Gpio objects.
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    led = new Gpio(14, 'out'), // Export GPIO #14 as an output.
+    iv;
+
+  // Toggle the state of the LED on GPIO #14 every 200ms.
+  // Here synchronous methods are used. Asynchronous methods are also available.
+  iv = setInterval(function() {
+    led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
+  }, 200);
+
+  // Stop blinking the LED and turn it off after 5 seconds.
+  setTimeout(function() {
+    clearInterval(iv); // Stop blinking
+    led.writeSync(0); // Turn LED off.
+    led.unexport(); // Unexport GPIO and free resources
+  }, 5000);
+
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
