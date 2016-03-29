6d13290dcab73ed4cbe122ccac4127159150205d
Brandon H Bodine <brandonhbodine@gmail.com>
2016-03-28 18:26:26 -0600

    Added back port.env

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ function alarmCheck(dayNow, hourNow, minutesNow, ampmNow) {
 function postLedOn() {
   var options = {
     hostname: 'localhost',
-    port: '443',
+    port: process.env.PORT,
     path:'/ledControls/on',
     method: 'POST',
     headers: {
@@ -164,7 +164,7 @@ function postLedOn() {
 function postMp3On() {
   var options = {
     hostname: 'localhost',
-    port: '443',
+    port: process.env.PORT,
     path:'/mp3Controls/on',
     method: 'GET',
     headers: {
