252ce6ac97292648384c1fc87e3acefe5255499d
Brandon H Bodine <brandonhbodine@gmail.com>
2016-03-29 23:02:24 -0600

    Added random song fuctionality

--- routes/mp3Controls.js
+++ routes/mp3Controls.js
@@ -3,8 +3,7 @@ var express = require('express');
 var router = express.Router();
 var Player = require('player');
 
-// create player instance
-var player = new Player('./mp3/CircleOfLife.mp3');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
@@ -14,6 +13,9 @@ router.get('/', function(req, res) {
 
 /* Testing route to turn on music */
 router.get('/on', function(req, res) {
+  var songFile = randomSong();
+  // create player instance
+  var player = new Player(songFile);
   player.play();
   res.render('index', {
     title: 'MP3 ON!',
@@ -28,5 +30,29 @@ router.get('/off', function(req, res) {
     title: 'MP3 OFF!',
   });
 });
+
+// Random song picker
+
+function randomSong(){
+  var song = getRandomIntInclusive(1,4);
+  var dir = './mp3/'
+  // Defualt song becuase tired
+  var title = 'CircleOfLife.mp3';
+  if (song === 1) {
+    title = 'FlightOfTheConchords-IfYoureIntoIt.mp3';
+  } else if ( song === 2 ) {
+    title = 'LadyGaga-Alejandro.mp3';
+  } else if ( song === 3 ) {
+    title = 'SystemOfADown-ChopSuey.mp3';
+  } else {
+    title = 'The1975-TheSound.mp3';
+  }
+  console.log('Playing ' + title);
+  return dir + title;
+}
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 module.exports = router;
